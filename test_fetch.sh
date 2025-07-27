aa5aa7aaf0a1b663a5e2e94743163e761bad35aa
Michal Trybus <komar007@gmail.com>
2025-07-27 18:00:02 +0200

    test: added a fetch test
    This one verifies that a generated cargo project would build without
    network access after lock-fetch. cargo build is not actually used, cargo
    fetch --frozen seems to be enough.

---
+++ test_fetch.sh
@@ -0,0 +1,35 @@
+#!/bin/sh
+
+set -e
+
+if [ -z "$1" ]; then
+	echo "usage: $0 crate..."
+	exit 1
+fi
+
+cargo -q build
+TARGET=$(realpath target/debug/cargo-lock-fetch)
+
+D=$(mktemp -d)
+cleanup() {
+    rm -rf "$D"
+}
+trap cleanup EXIT
+
+mkdir "$D/package"
+
+cd "$D/package"
+cargo -q init . --vcs none --name package
+cargo -q add "$@"
+
+cd - > /dev/null
+export CARGO_HOME="$D/cargo_home"
+$TARGET lock-fetch -q --lockfile-path "$D/package/Cargo.lock"
+cd "$D/package"
+
+if cargo -q fetch --frozen; then
+	echo "pass: fetched $(find "$D/cargo_home/registry/cache/" -maxdepth 2 -type f | wc -l) crates"
+else
+	echo fail
+	exit 1
+fi
